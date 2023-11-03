9d23fd462affd3b76ddcd6e53f108335e19cbb1c
jefkruz <josephugbeva@gmail.com>
2023-11-03 05:31:51 +0100

    ccorrected drinks

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -101,15 +101,8 @@ class AdminController extends Controller
     {
 
         $data['page_title'] = 'All Directors';
-//        $users = Admin::where('role_id',6)->get();
         $data['users'] = Admin::where('role_id',6)->get();
-//        $payload = [];
-//
-//        foreach($users as $g){
-//            array_push($payload, $g);
-//        }
 
-//        $data['users'] = $payload;
         return view('admins.index',$data);
     }
 
