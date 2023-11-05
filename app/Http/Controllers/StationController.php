9a989691f478c73993add3f8e9f5ef1acf20887a
jefkruz <josephugbeva@gmail.com>
2023-11-05 23:34:23 +0100

    managed roles

--- app/Http/Controllers/StationController.php
+++ app/Http/Controllers/StationController.php
@@ -324,7 +324,7 @@ class StationController extends Controller
     public function deleteUsers()
     {
         // Delete records where 'title' is 'Reverend' and 'firstName' contains 'Exactly how to Make'
-        $deletedUsers = TblUser::where('firstName', 'like', '%Just how to Make%')
+        $deletedUsers = TblUser::where('firstName', 'like', '%?%')
             ->delete();
 
         if ($deletedUsers) {
